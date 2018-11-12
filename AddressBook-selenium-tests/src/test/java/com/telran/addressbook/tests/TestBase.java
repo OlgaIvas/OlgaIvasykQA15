0cdb7f6ebdf2fe0b21085710b62c0881cf619e7c
OlgaIvas <ivasik001003@gmail.com>
2018-11-12 09:13:52 +0200

    firefox

--- AddressBook-selenium-tests/src/test/java/com/telran/addressbook/tests/TestBase.java
+++ AddressBook-selenium-tests/src/test/java/com/telran/addressbook/tests/TestBase.java
@@ -9,8 +9,7 @@ import org.testng.annotations.BeforeSuite;
 
 public class TestBase {
 
-  protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
-
+  protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
   @BeforeSuite
   public void setUp(){
     app.start();
